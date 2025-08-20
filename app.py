6c1690245095d3ca8b1d7dfdadd9eb034d318265
Devesh Sharma <kaluklt1496@gmail.com>
2025-08-20 13:00:43 +0530

    third commit - quiz toml final version

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # --------------------- Firebase Initialization ---------------------
+# Load Firebase credentials from Streamlit secrets
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+    cred = credentials.Certificate(dict(st.secrets["firebase"]))
     firebase_admin.initialize_app(cred)
+
 db = firestore.client()
 
 # --------------------- Utilities ---------------------
